54a3d049ed655ba7df6631235d9aed6532ad5229
unknown <prasadaklakshwanth@gmail.com>
2022-01-12 16:17:22 +0530

    Changed Color

--- src/components/gallery.jsx
+++ src/components/gallery.jsx
@@ -15,8 +15,8 @@ export const Gallery = (props) => {
           <div className='portfolio-items'>
             {props.data
               ? props.data.map((d, i) => (
-                <div key={`${d.title}-${i}`} className='col-sm-6 col-md-4 col-lg-4'>
-                  <Image title={d.title} largeImage={d.largeImage} smallImage={d.smallImage} />
+                <div key={`${d.title}-${i},${d.desc}-${i}`} className='col-sm-6 col-md-4 col-lg-4'>
+                  <Image title={d.title}largeImage={d.largeImage} smallImage={d.smallImage} />
                 </div>
               ))
               : 'Loading...'}
